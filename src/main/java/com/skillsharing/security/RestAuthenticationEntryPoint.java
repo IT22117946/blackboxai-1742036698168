b3f47b268f69556c198ec714c1f8a1917b2f91d4
IT22117946 <127727856+IT22117946@users.noreply.github.com>
2025-03-15 16:36:04 +0530

    Add src/main/java/com/skillsharing/security/RestAuthenticationEntryPoint.java 1742036762918

---
+++ src/main/java/com/skillsharing/security/RestAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package com.skillsharing.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                        HttpServletResponse response,
+                        AuthenticationException authException) throws IOException, ServletException {
+        
+        log.error("Responding with unauthorized error. Message - {}", authException.getMessage());
+        
+        // Send a JSON response with 401 status
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getOutputStream().println("{ \"error\": \"" + authException.getMessage() + 
+                                        "\", \"message\": \"You need to login to access this resource\" }");
+    }
+}
